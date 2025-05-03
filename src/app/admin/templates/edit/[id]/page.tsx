2028d35f46b6918d71fa53962692588efe0bffa4
Siva-OO7 <sivaramakrishnmag253@gmail.com>
2025-05-03 10:46:44 +0530

    types checking

--- src/app/admin/templates/edit/[id]/page.tsx
+++ src/app/admin/templates/edit/[id]/page.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import TemplateForm from "../../_components/template-form";
 import { getTemplateById } from "@/server-actions/templates";
+import { PageProps } from 'next/types';
 
-async function EditTemplate({params} : {
-  params : {
-    id : string;
-  }
-}) {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+async function EditTemplate({ params }: PageProps<Props>) {
   const response = await getTemplateById(params.id);
-  if(!response.success) {
-    return <div>{response.message}</div>
+  if (!response.success) {
+    return <div>{response.message}</div>;
   }
   return (
     <div>
       <h1 className="text-xl font-bold text-primary">Edit Template</h1>
-      <TemplateForm 
-       initialValues={response.data}
-       type="edit"
+      <TemplateForm
+        initialValues={response.data}
+        type="edit"
       />
     </div>
   );
